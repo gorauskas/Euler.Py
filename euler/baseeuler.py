147ea341e80e001fc456a4f8dce72f11e5c182af
Jonas Gorauskas <jgorauskas@gmail.com>
2014-11-11 20:19:24 -0800

    adding abstract base class

---
+++ euler/baseeuler.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import abc
+
+
+class BaseEuler(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def solve(self):
+        raise NotImplementedError
+
+    @abc.abstractproperty
+    def problem(self):
+        raise NotImplementedError
