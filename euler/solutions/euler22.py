306777844845f5052f64e63d9974f673a272dd55
Jonas Gorauskas <jgorauskas@gmail.com>
2014-12-03 19:48:36 -0800

    solution 22 skeleton; names.txt data file for problem 22;

---
+++ euler/solutions/euler22.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from euler.baseeuler import BaseEuler
+
+
+class Euler(BaseEuler):
+    def solve(self):
+        pass
+
+    @property
+    def problem(self):
+        return '''
+
+'''
