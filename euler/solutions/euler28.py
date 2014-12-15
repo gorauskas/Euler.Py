bd1be8edf5d3be14290d1711a028fe2c9d2e10d7
Jonas Gorauskas <jgorauskas@gmail.com>
2014-12-14 19:41:00 -0800

    adding solution to problem 28

---
+++ euler/solutions/euler28.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from euler.baseeuler import BaseEuler
+
+
+'''
+You don't have to actually construct the spiral to solve this problem.
+this is a simple summation problem and therefore the result can be expressed
+by a formula which is presented below
+'''
+
+
+class Euler(BaseEuler):
+    def solve(self):
+        n = 500  # we focus on the rings => a 1001x1001 spiral has 500 rings
+        res = ((16 * (n ** 3) + 26 * n) / 3 + 10 * (n ** 2)) + 1
+        print('The sum of the numbers on the diagonals in a 1001 by 1001 ' +
+              'spiral is %d' % res)
+
+    @property
+    def problem(self):
+        return '''
+Project Euler Problem 28:
+
+    Starting with the number 1 and moving to the right in a clockwise direction
+    a 5 by 5 spiral is formed as follows:
+
+        21 22 23 24 25
+        20  7  8  9 10
+        19  6  1  2 11
+        18  5  4  3 12
+        17 16 15 14 13
+
+    It can be verified that the sum of the numbers on the diagonals is 101.
+
+    What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral
+    formed in the same way?
+'''
