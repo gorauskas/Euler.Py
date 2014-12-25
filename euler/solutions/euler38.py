38bae8b22f36eb57be351ca54905de737fa037d7
Jonas Gorauskas <jgorauskas@gmail.com>
2014-12-24 22:13:28 -0800

    adding solution to problem 38

---
+++ euler/solutions/euler38.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from euler.baseeuler import BaseEuler
+from euler.util import is_pandigital
+
+
+class Euler(BaseEuler):
+    def solve(self):
+        for n in range(9487, 9213, -1):
+            p = str(n*1) + str(n*2)
+            if is_pandigital(p):
+                break
+
+        print('The largest 1 to 9 pandigital 9-digit number is %d' % int(p))
+
+    @property
+    def problem(self):
+        return '''
+Project Euler Problem 38:
+
+    Take the number 192 and multiply it by each of 1, 2, and 3:
+
+        192 * 1 = 192
+        192 * 2 = 384
+        192 * 3 = 576
+
+    By concatenating each product we get the 1 to 9 pandigital, 192384576. We
+    will call 192384576 the concatenated product of 192 and (1,2,3)
+
+    The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4,
+    and 5, giving the pandigital, 918273645, which is the concatenated product
+    of 9 and (1,2,3,4,5).
+
+    What is the largest 1 to 9 pandigital 9-digit number that can be formed as
+    the concatenated product of an integer with (1,2, ... , n) where n > 1?
+'''
