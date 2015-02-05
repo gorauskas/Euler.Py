9eac96be77b4c7b4b159561cc6f7ea8902688c41
Jonas Gorauskas <jgorauskas@gmail.com>
2015-02-04 18:26:39 -0800

    adding support for tests to problem 22

--- euler/solutions/euler22.py
+++ euler/solutions/euler22.py
@@ -17,8 +17,12 @@ class Euler(BaseEuler):
             names = sorted([name for name
                             in f.read().replace('"', '').split(',')])
 
-        print('The total of all the name scores in the file is: %d'
-              % sum([get_name_score(names, name) for name in names]))
+        return sum([get_name_score(names, name) for name in names])
+
+    @property
+    def answer(self):
+        return ('The total of all the name scores in the file is: %d'
+                % self.solve())
 
     @property
     def problem(self):
@@ -32,8 +36,8 @@ Project Euler Problem 22:
     name score.
 
     For example, when the list is sorted into alphabetical order, COLIN, which
-    is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN
-    would obtain a score of 938 * 53 = 49714.
+    is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So,
+    COLIN would obtain a score of 938 * 53 = 49714.
 
     What is the total of all the name scores in the file?
 '''
