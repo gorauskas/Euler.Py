b60a1f0dd4a179cdb4404dab4c2f7b1305f6ad4c
Jonas Gorauskas <jgorauskas@gmail.com>
2015-04-01 18:00:00 -0800

    adding test for problem 15

---
+++ tests/test_euler15.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from euler.solutions.euler15 import Euler
+import unittest
+
+
+class TestEuler(unittest.TestCase):
+    def setUp(self):
+        self.fixture = Euler()
+
+    def tearDown(self):
+        del self.fixture
+
+    def test_solve(self):
+        self.assertEqual(self.fixture.solve(), 137846528820)
