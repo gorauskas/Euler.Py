333f42557ce243e33b9cf36fba99b89440cc56db
Jonas Gorauskas <jgorauskas@gmail.com>
2017-05-20 17:12:42 -0800

    adding test for problem 63

---
+++ tests/test_euler63.py
@@ -0,0 +1,16 @@
+
+# -*- coding: utf-8 -*-
+
+from euler.solutions.euler63 import Euler
+import unittest
+
+
+class TestEuler(unittest.TestCase):
+    def setUp(self):
+        self.fixture = Euler()
+
+    def tearDown(self):
+        del self.fixture
+
+    def test_solve(self):
+        self.assertEqual(self.fixture.solve(), 49)
